1ac9f79154e530ead0e2789b00b3b61089989069
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-06-20 03:12:55 +0500

    Insert Data into MongoDB using Mongoose

--- mongodb/src/app.module.ts
+++ mongodb/src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
-    StudentModule
+    ConfigModule.forRoot(), // Loads .env
+    MongooseModule.forRoot(process.env.MONGO_URI!), 
+    StudentModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- mongodb/src/student/student.schema.ts
+++ mongodb/src/student/student.schema.ts
@@ -1,17 +1,18 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
 export type StudentDocument = Student & Document;
 
-@Schema({timestamps: true})
-export class Student{
-    @Prop({required: true})
-    name: string;
+@Schema({ timestamps: true })
+export class Student {
+  @Prop({ required: true })
+  name: string;
 
-    @Prop({required: true})
-    age: number;
+  @Prop({ required: true })
+  age: number;
 
-    @Prop()
-    email?: string;
+  @Prop()
+  email?: string;
 }
 
-export const StudentSchema = SchemaFactory.createForClass(Student)
+export const StudentSchema = SchemaFactory.createForClass(Student);
